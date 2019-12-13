c5ed0d2c4dc4f7637762a7c7b749b7f70d328eae
Muhammad Talha <talha8018@gmail.com>
2019-12-14 00:41:24 +0500

    template setup is done

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify,render_template
 from flaskext.mysql import MySQL
 
+
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -9,17 +10,22 @@ app.config['MYSQL_DATABASE_DB'] = 'rnc'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-conn = mysql.connect()
-cursor = conn.cursor()
-
-cursor.execute("SELECT * from users")
-data = cursor.fetchall()
+# conn = mysql.connect()
+# cursor = conn.cursor()
+#
+# cursor.execute("SELECT * from users")
+# data = cursor.fetchall()
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def index():
     return render_template('login.html')
+
+
+@app.route('/hello')
+def hello():
+    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
